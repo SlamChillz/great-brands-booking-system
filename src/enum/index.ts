d0924abda7f7182779751f53267d77b5e4bbeb17
SlamChillz <slamchillz@gmail.com>
2025-02-10 00:32:41 +0100

    enums

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,4 +1,19 @@
 export enum BookingStatus {
   BOOKED = 'booked',
   CANCELLED = 'cancelled',
+  WAITING = 'waiting',
+  FAILED = 'failed',
+}
+
+export enum EventTrigger {
+  BOOK = 'book',
+  CANCEL = 'cancel',
+  INIT = 'init',
+  REPLACE = 'replace',
+  WAIT = 'wait',
+}
+
+export enum ServiceCart {
+  USER = 'user',
+  EVENT = 'event',
 }
