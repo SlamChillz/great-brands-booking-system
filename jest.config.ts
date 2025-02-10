86cb5232bbf5dbcd5a73629014a4e13fd30c0f68
SlamChillz <slamchillz@gmail.com>
2025-02-10 10:27:59 +0100

    add coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,15 @@ const config: Config = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  coverageReporters: ['text', 'html', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: "jest-environment-node",
   setupFilesAfterEnv: ["./test/setup.ts"],
